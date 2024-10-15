e09eefb4037cbc09def7128ddc09cc21ef67e31e
calioralam <121493904+calioralam@users.noreply.github.com>
2024-10-16 01:22:07 +0700

    update2

--- admin-ui/src/rangkuman.jsx
+++ admin-ui/src/rangkuman.jsx
@@ -10,7 +10,7 @@ function App() {
     <>
       <div>
         <h1>Selamat Datang</h1>
-        <button><a href="rangkuman.jsx">Data Rangkuman</a></button>
+        <button><a href="rangkuman.html">Data Rangkuman</a></button>
         <button><a href="pengiriman.html">Pengriman Surat</a></button>
         <button><a href="pemasukan.html">Data pemasukan</a></button>
       </div>
